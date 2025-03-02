9ae8ea58edba884cb65bee1b64eab10bdd4423ab
leun <leun12968@gmail.com>
2025-03-03 08:01:52 +0900

    feat: add Task entity

---
+++ server/src/main/java/com/leun/task/entity/Task.java
@@ -0,0 +1,36 @@
+package com.leun.task.entity;
+
+import com.leun.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    private String title;
+
+    private String description;
+
+    private LocalDateTime dueDate;
+
+    private Boolean isAllDay;
+
+    private Boolean isCompleted;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+}
