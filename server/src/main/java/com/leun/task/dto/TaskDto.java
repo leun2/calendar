35bff9ba0eba299040bc6ecf84aa59f9a7aba301
leun <leun12968@gmail.com>
2025-03-05 00:52:26 +0900

    feat: add TaskDto

---
+++ server/src/main/java/com/leun/task/dto/TaskDto.java
@@ -0,0 +1,17 @@
+package com.leun.task.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class TaskDto {
+
+    @Getter
+    @Setter
+    public static class Get {
+    }
+
+    @Getter
+    @Setter
+    public static class Post {
+    }
+}
