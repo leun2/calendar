46522ead8997b6604523e56b6dbebe71a2be99d6
leun <leun12968@gmail.com>
2025-03-05 00:52:18 +0900

    feat: add EventDto

---
+++ server/src/main/java/com/leun/event/dto/EventDto.java
@@ -0,0 +1,17 @@
+package com.leun.event.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class EventDto {
+
+    @Getter
+    @Setter
+    public static class Get {
+    }
+
+    @Getter
+    @Setter
+    public static class Post {
+    }
+}
