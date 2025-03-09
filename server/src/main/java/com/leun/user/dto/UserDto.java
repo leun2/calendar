63e4a308c949a14f02b88fe0a93635dbffeb2d17
leun <leun12968@gmail.com>
2025-03-09 10:43:10 +0900

    feat: add UserDto

--- server/src/main/java/com/leun/user/dto/UserDto.java
+++ server/src/main/java/com/leun/user/dto/UserDto.java
@@ -1,4 +1,18 @@
 package com.leun.user.dto;
 
+import jakarta.validation.constraints.Email;
+import lombok.Getter;
+import lombok.Setter;
+
 public class UserDto {
+
+    @Getter
+    @Setter
+    public static class Request {
+
+        @Email
+        private String email;
+        private String password;
+        private String name;
+    }
 }
