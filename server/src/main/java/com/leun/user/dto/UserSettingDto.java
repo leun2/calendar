aec6cb447f68ce9f27fc268f72a89604b0002d11
leun <leun12968@gmail.com>
2025-03-09 10:43:33 +0900

    feat: add UserSettingDto

--- server/src/main/java/com/leun/user/dto/UserSettingDto.java
+++ server/src/main/java/com/leun/user/dto/UserSettingDto.java
@@ -7,11 +7,36 @@ public class UserSettingDto {
 
     @Getter
     @Setter
-    public static class Get {
+    public static class Response {
+        private String language;
+        private String country;
+        private String timezone;
+
+        public Response(String language, String country, String timezone) {
+            this.language = language;
+            this.country = country;
+            this.timezone = timezone;
+        }
     }
 
-    @Getter
-    @Setter
-    public static class Post {
+    public static class Request {
+
+        @Getter
+        @Setter
+        public static class Language {
+            private String language;
+        }
+
+        @Getter
+        @Setter
+        public static class Country {
+            private String country;
+        }
+
+        @Getter
+        @Setter
+        public static class Timezone {
+            private String timezone;
+        }
     }
 }
