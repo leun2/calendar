8207fee2171c9fa21e966131977f3a6b5658dd34
leun <leun12968@gmail.com>
2025-03-09 10:48:12 +0900

    feat: add queries for user delete by email

--- server/src/main/java/com/leun/user/repository/UserRepository.java
+++ server/src/main/java/com/leun/user/repository/UserRepository.java
@@ -1,11 +1,20 @@
 package com.leun.user.repository;
 
 import com.leun.user.entity.User;
+import jakarta.transaction.Transactional;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM User u WHERE u.email = :email")
+    void deleteUserByEmail(@Param("email") String email);
 }
