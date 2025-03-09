1984de3720ea965bb1da00643f09e515680abebc
leun <leun12968@gmail.com>
2025-03-09 10:40:51 +0900

    feat: add CalendarDto

--- server/src/main/java/com/leun/calendar/dto/CalendarDto.java
+++ server/src/main/java/com/leun/calendar/dto/CalendarDto.java
@@ -1,5 +1,21 @@
 package com.leun.calendar.dto;
 
+import com.leun.event.dto.EventDto;
+import com.leun.event.dto.EventDto.Response;
+import com.leun.task.dto.TaskDto;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class CalendarDto {
 
+    private List<EventDto.Response> events;
+    private List<TaskDto.Response> tasks;
+
+    public CalendarDto(List<Response> events, List<TaskDto.Response> tasks) {
+        this.events = events;
+        this.tasks = tasks;
+    }
 }
