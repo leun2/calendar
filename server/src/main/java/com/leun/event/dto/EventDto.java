ab637b169868f400832e6de7b3028ced77bce55f
leun <leun12968@gmail.com>
2025-03-09 10:41:07 +0900

    feat: add EventDto

--- server/src/main/java/com/leun/event/dto/EventDto.java
+++ server/src/main/java/com/leun/event/dto/EventDto.java
@@ -1,5 +1,7 @@
 package com.leun.event.dto;
 
+import com.leun.event.entity.Event;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,11 +9,48 @@ public class EventDto {
 
     @Getter
     @Setter
-    public static class Get {
+    public static class Response {
+        private Long id;
+        private String title;
+        private String description;
+        private String location;
+        private LocalDateTime startTime;
+        private LocalDateTime endTime;
+        private String color;
+
+        public Response(Long id, String title, String description, String location,
+            LocalDateTime startTime,
+            LocalDateTime endTime, String color) {
+            this.id = id;
+            this.title = title;
+            this.description = description;
+            this.location = location;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.color = color;
+        }
+
+        public static Response fromEntity(Event event) {
+            return new Response(
+                event.getId(),
+                event.getTitle(),
+                event.getDescription(),
+                event.getLocation(),
+                event.getStartTime(),
+                event.getEndTime(),
+                String.valueOf(event.getColor())
+            );
+        }
     }
 
     @Getter
     @Setter
-    public static class Post {
+    public static class Request {
+        private String title;
+        private String description;
+        private String location;
+        private LocalDateTime startTime;
+        private LocalDateTime endTime;
+        private String color;
     }
 }
