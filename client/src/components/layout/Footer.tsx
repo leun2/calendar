747d26b2358206f5c3394aea2b2587ba0026b2aa
leun <leun12968@gmail.com>
2025-04-06 23:17:58 +0900

    feat: create footer component

---
+++ client/src/components/layout/Footer.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import 'styles/styles.css'
+
+const Footer: React.FC = () => {
+
+
+  return (
+    <div className='footer' />
+  );
+};
+
+export default Footer;
