472e2d0d481dd423cbd93db6f6c325c21efc4f93
leun <leun12968@gmail.com>
2025-04-06 23:20:42 +0900

    feat: set up routing for main views and auth pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,26 @@
-import Header from 'components/layout/Header';
+import Main from "pages/Main";
+import Settings from "pages/Settings";
+import SignIn from "pages/SignIn";
+import SignUp from "pages/SignUp";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+
   return (
-    <div>
-      <Header />
+    <div className='app'>
+      
+      <Router>
+        <Routes>
+          {/* 기본 경로를 /day로 리디렉트 */}
+          <Route path="/" element={<Navigate replace to="/day" />} />
+          <Route path="/:viewType" element={<Main />} />
+          <Route path="/:viewType/:year/:month/:day" element={<Main />} />
+          <Route path="/settings" element={<Settings />}/>
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+
     </div>
   );
 }
