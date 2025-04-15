b76958996a98cae2fa8f5326289f03f3f62f0652
leun <leun12968@gmail.com>
2025-04-16 02:03:19 +0900

    feat: add authenticated routes and apply axios interceptors globally

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,25 +2,54 @@ import Main from "pages/Main";
 import Settings from "pages/Settings";
 import SignIn from "pages/SignIn";
 import SignUp from "pages/SignUp";
+import Error from 'pages/Error';
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { setupInterceptors } from "api/setupInterceptors";
+import { AuthProvider } from 'components/auth/AuthContext';
+import AuthenticatedRoute from 'components/auth/AuthenticatedRoute';
+
+setupInterceptors(); // 전역 설정 딱 한 번!
 
 function App() {
 
   return (
     <div className='app'>
       
-      <Router>
-        <Routes>
-          {/* 기본 경로를 /day로 리디렉트 */}
-          <Route path="/" element={<Navigate replace to="/day" />} />
-          <Route path="/:viewType" element={<Main />} />
-          <Route path="/:viewType/:year/:month/:day" element={<Main />} />
-          <Route path="/settings" element={<Settings />}/>
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
-        </Routes>
-      </Router>
+        <AuthProvider>
+            <Router>
+                <Routes>
+                    <Route 
+                        path="/" 
+                        element={<Navigate replace to="/day" />} />
 
+                    <Route 
+                        path="/:viewType" 
+                        element={
+                            <AuthenticatedRoute>
+                                <Main />
+                            </AuthenticatedRoute>} />
+                        
+                    <Route path="/:viewType/:year/:month/:day" 
+                        element={
+                            <AuthenticatedRoute>
+                                <Main />
+                            </AuthenticatedRoute>} />
+
+                    <Route 
+                        path="/settings" 
+                        element={
+                            <AuthenticatedRoute>
+                                <Settings />
+                            </AuthenticatedRoute>}/>
+
+                    <Route path="/signin" element={<SignIn />} />
+
+                    <Route path="/signup" element={<SignUp />} />
+
+                    <Route path='*' element={<Error />} />
+                </Routes>
+            </Router>
+        </AuthProvider>
     </div>
   );
 }
