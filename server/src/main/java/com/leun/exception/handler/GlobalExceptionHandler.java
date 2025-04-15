ba0fac648ca66faa6fda9e2b284d5391e2d8cab2
leun <leun12968@gmail.com>
2025-04-16 02:16:24 +0900

    feat: add exception handling for invalid HTTP message and illegal arguments

--- server/src/main/java/com/leun/exception/handler/GlobalExceptionHandler.java
+++ server/src/main/java/com/leun/exception/handler/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import com.leun.exception.Response.ErrorResponse;
 import com.leun.exception.UnauthorizedAccessException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -26,5 +28,22 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public ErrorResponse InvalidDateException(InvalidDateException e) {
         return new ErrorResponse("Invalid Date", e.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadable(HttpMessageNotReadableException e) {
+        return ResponseEntity.badRequest().body("요청 형식이 잘못되었습니다: " + e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException e) {
+        // 400 Bad Request로 응답을 반환
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 요청입니다: " + e.getMessage());
+    }
+
+    @ExceptionHandler(EnumConstantNotPresentException.class)
+    public ResponseEntity<String> handleEnumException(EnumConstantNotPresentException e) {
+        // 400 Bad Request로 응답을 반환
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("잘못된 색상 값입니다: " + e.getMessage());
     }
 }
