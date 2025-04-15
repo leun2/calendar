d797d421dfea901df3dcdf8a1bbcc21b2ffd30e3
leun <leun12968@gmail.com>
2025-04-16 02:39:04 +0900

    feat: create Settings component to display settings page

---
+++ client/src/pages/Settings.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'styles/styles.css'
+
+function Settings() {
+
+
+  return (
+    <div className='settings'>
+        settings
+    </div>
+  );
+}
+
+export default Settings;
