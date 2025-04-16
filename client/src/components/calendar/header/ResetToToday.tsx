4d53105d372443ec4eb6bfc8b848ab59f75afb40
leun <leun12968@gmail.com>
2025-04-16 14:52:44 +0900

    feat: add Reset to Today button for quick navigation to current date

--- client/src/components/calendar/header/ResetToToday.tsx
+++ client/src/components/calendar/header/ResetToToday.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+
 import 'styles/header/reset-to-today.css'
 
 const ResetToday: React.FC = () => {
+
+  const { viewType } = useParams();
+
+  const navigate = useNavigate();
+
   const handleClick = () => {
-    alert('Go to today');
+    navigate(`/${viewType}`);
   };
 
   return (
     <div>
-      <button className="today-button">
+      <button className="today-button" onClick={() => handleClick()}>
         <span>오늘</span>
         <div className="tooltip" role="tooltip" aria-hidden="true">today</div>
       </button>
