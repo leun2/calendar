f25c50dd561efc60c82a3391d8c1cee0428fc1e8
leun <leun12968@gmail.com>
2025-04-16 15:05:56 +0900

    feat: implement calendar header with navigation, dropdowns, and date controls

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -6,12 +6,15 @@ import DropDown from 'components/calendar/header/SettingsDropDown';
 import logo from '../../assets/images/logo.png';
 import 'styles/styles.css'
 import ResetToday from 'components/calendar/header/ResetToToday';
+import 'styles/font.css'
+import { useNavigate } from 'react-router-dom';
 
 interface HeaderProps {
 	navigateToView: (view: 'day' | 'week' | 'month' | 'year', date?:Date) => void;
 }
 
 const Header: React.FC<HeaderProps> = ({ navigateToView }) => {
+	// const navigate = useNavigate();
 
 	const [openDropdown, setOpenDropdown] = useState<string | null>(null);
 
@@ -19,28 +22,35 @@ const Header: React.FC<HeaderProps> = ({ navigateToView }) => {
 		setOpenDropdown(openDropdown === dropdown ? null : dropdown); // 클릭된 드롭다운만 열리게 설정
 	};
 
+	function handleNavigateRoute() {
+		// navigate(`/`);
+	}
+
 	return (
-		<header className="header">
-			<div className="image-container">
+		<header className="header" style={{background:""}}>
+			{/* <div className="image-container">
 				<img src={logo} alt="logo" />
+			</div> */}
+			<div className='logo-container' style={{ fontWeight:"400", fontSize:"24px", cursor:"default" }} onClick={handleNavigateRoute}>
+				calendar
 			</div>
-			<span style={{ fontWeight:"400", fontSize:"24px", paddingLeft:"10px" }}>Calendar</span>
-			<div style={{paddingLeft:"30px" }}>
+
+			<div style={{marginLeft:"100px" }}>
 				<ResetToday />
 			</div>
-			<div style={{paddingLeft:"30px"}}>
+			<div style={{marginLeft:"30px"}}>
 				<DateShifter />
 			</div>
-			<div style={{paddingLeft:"15px" , fontWeight:"400", fontSize:"24px"}}>
+			<div style={{marginLeft:"15px" , fontWeight:"400", fontSize:"24px"}}>
 				<DateDisplay />
 			</div>
-			<div  style={{paddingLeft:"350px" }}>
+			<div  style={{marginLeft:"350px" }}>
 			<DropDown
 				isOpen={openDropdown === "dropDown"}
 				onToggle={() => handleToggleDropdown("dropDown")}
 			/>
 			</div>
-			<div  style={{paddingLeft:"5px" }}>
+			<div  style={{marginLeft:"5px" }}>
 			<ViewSelector
 				isOpen={openDropdown === "viewSelector"}
 				onToggle={() => handleToggleDropdown("viewSelector")}
