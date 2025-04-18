04faa2d9117a44f38e4f20dc992a35f9f2320124
leun <leun12968@gmail.com>
2025-04-19 00:28:23 +0900

    feat: add CORS config and permit Google login endpoint

--- server/src/main/java/com/leun/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/leun/auth/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 import com.leun.auth.security.JwtAuthenticationFilter;
 import com.leun.auth.security.JwtUtil;
 import com.leun.auth.service.CustomUserDetailsService;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,6 +18,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -28,19 +32,35 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .httpBasic(withDefaults())
-            .cors(withDefaults())
+            .httpBasic(AbstractHttpConfigurer::disable)
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .csrf(AbstractHttpConfigurer::disable)
             .sessionManagement(
                 session ->
                     session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            ).authorizeHttpRequests(auth -> auth
-                .requestMatchers("/v1/auth/login", "/v1/user").permitAll()
+            )
+            .formLogin(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/v1/auth/login", "/v1/ping", "/v1/user", "/v1/auth/google/login").permitAll()
                 .anyRequest().authenticated())
             .addFilterBefore(
                 new JwtAuthenticationFilter(jwtUtil, userDetailsService),
                 UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(
+            Arrays.asList("http://localhost:3000", "http://calendar-front-dev.s3-website.ap-northeast-2.amazonaws.com")); // React 앱 도메인
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
     @Bean
