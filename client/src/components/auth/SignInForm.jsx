42f861dc5740e07e7574594ecd3490ae8c3199b2
leun <leun12968@gmail.com>
2025-04-19 00:47:16 +0900

    chore: remove debug console.log statement

--- client/src/components/auth/SignInForm.jsx
+++ client/src/components/auth/SignInForm.jsx
@@ -41,7 +41,6 @@ function SignInForm({ onPrev, email }) {
             const success = await authContext.login(email, values.password)
             if (success) navigate('/');
             else {
-                console.log("qweqweq")
                 formik.setErrors({
                     password: '아이디 또는 비밀번호가 잘못 되었습니다. 아이디와 비밀번호를 정확히 입력해 주세요'
                 });
