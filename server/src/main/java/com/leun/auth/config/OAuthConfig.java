6b274e26cc21845b40b05eade5cd828f0ad58bd1
leun <leun12968@gmail.com>
2025-04-28 01:37:04 +0900

    chore: add OAuth configuration class

---
+++ server/src/main/java/com/leun/auth/config/OAuthConfig.java
@@ -0,0 +1,19 @@
+package com.leun.auth.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Getter
+public class OAuthConfig {
+
+    @Value("${spring.security.oauth2.client.registration.google.client-id}")
+    private String clientId;
+
+    @Value("${spring.security.oauth2.client.registration.google.client-secret}")
+    private String clientSecret;
+
+    @Value("${spring.security.oauth2.client.registration.google.redirect-uri}")
+    private String redirectUri;
+}
